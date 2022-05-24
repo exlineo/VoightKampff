389aae5ebdb0b5d85cf874f6d9d48a4625a431af
exlineo <contact@exlineo.com>
2022-05-24 17:22:45 +0200

    Ajout de Firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
+  firebase: {
+    projectId: 'firevoightkampff',
+    appId: '1:173254517285:web:fbb421017490beb19b15cc',
+    databaseURL: 'https://firevoightkampff-default-rtdb.europe-west1.firebasedatabase.app',
+    storageBucket: 'firevoightkampff.appspot.com',
+    locationId: 'europe-west',
+    apiKey: 'AIzaSyBJaccAxL2rKaD5b4Cy8zZCXT1HpZDriE4',
+    authDomain: 'firevoightkampff.firebaseapp.com',
+    messagingSenderId: '173254517285',
+  },
   production: true
 };
