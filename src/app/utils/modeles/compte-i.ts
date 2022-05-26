d40f7e8450f62932fdcbfa6500334ad4b6fca4f8
exlineo <contact@exlineo.com>
2022-05-27 01:41:27 +0200

    Le temps réel fonctionne

--- src/app/utils/modeles/compte-i.ts
+++ src/app/utils/modeles/compte-i.ts
@@ -1,7 +1,13 @@
+import { ReponseI } from "./question-i";
+
 export interface CompteI {
   uid:string;
   nom?:string;
   email?:string;
   position?:{lat:number,long:number};
   statut:number;
+}
+export interface SuiviI{
+  uid:string;
+  reponses:Array<ReponseI>;
 }
--- src/app/utils/services/suivi.service.ts
+++ src/app/utils/services/suivi.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Database, objectVal, ref, set, list } from '@angular/fire/database';
-import { first } from 'rxjs/operators';
-import { CompteI } from '../modeles/compte-i';
+import { first, map } from 'rxjs/operators';
+import { CompteI, SuiviI } from '../modeles/compte-i';
 import { AuthService } from './auth.service';
 
 @Injectable({
@@ -9,7 +9,7 @@ import { AuthService } from './auth.service';
 })
 export class SuiviService {
 
-  suivi:any;
+  suivi:Array<any> = [];
 
 
   constructor(private rt:Database, private auth:AuthService) {
@@ -19,11 +19,13 @@ export class SuiviService {
   /** Surveillance des données en temps réel */
   /** Get realtime data */
   getRTDB() {
-    objectVal(ref(this.rt, 'users')).pipe(
-      // traceUntilFirst('database')
-      first()
-    ).subscribe(
-      d => console.log(d)
+    objectVal<any>(ref(this.rt, 'users')).subscribe(
+      d => {
+        console.log(d['1hq3qved9']);
+        for(let s in d){
+          this.suivi.push(d[s]);
+        }
+      }
     );
   }
   getReponses() {
@@ -31,7 +33,7 @@ export class SuiviService {
     // msg.valueChanges();
   }
   setRTDB(msg:any){
-    // msg['uid'] = this.auth.compte.uid;
+    msg['uid'] = this.auth.compte.uid;
     set(ref(this.rt, 'users/'+this.auth.compte.uid), msg);
   }
 }
